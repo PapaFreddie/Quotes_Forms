6cae5d192ce32f32db70134a7f65b510b4e92786
PapaFreddie <fred.papa@student.moringaschool.com>
2022-01-05 10:17:36 +0300

    added Quote.ts file, mock-quotes.ts file and quotes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { QuotesComponent } from './components/quotes/quotes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule
